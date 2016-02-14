0233be5de0bcb9e8934dc1d41b9b42e4754388da
Ron Toland <rontoland@gmail.com>
2016-02-14 09:33:34 -0600

    Fns to fetch book by title or title plus author

--- src/myshelf/core.clj
+++ src/myshelf/core.clj
@@ -108,3 +108,46 @@
          second
          :content
          (map extract-book-data))))
+
+(defn find-book-by-title
+  "Runs a query for a book by title. Returns only the last
+  20 results."
+  [consumer access-token title]
+  (let [search-url "https://www.goodreads.com/search/index"
+        params {:q title
+                :format "xml"
+                :field "title"}
+        resp (make-auth-request-GET consumer
+                                    access-token
+                                    search-url
+                                    params)]
+    (->> resp
+         :content
+         second
+         :content
+         (filter #(= :results (:tag %)))
+         first
+         :content
+         (map element->map)
+         (map (comp (juxt :title :id :author) :best_book :work)))))
+
+(defn find-book-by-title-and-author
+  "Runs a query for a book by title and author.
+  Returns only the last 20 results."
+  [consumer access-token title author]
+  (let [search-url "https://www.goodreads.com/search/index"
+        params {:q (str title " " author)
+                :format "xml"}
+        resp (make-auth-request-GET consumer
+                                    access-token
+                                    search-url
+                                    params)]
+    (->> resp
+         :content
+         second
+         :content
+         (filter #(= :results (:tag %)))
+         first
+         :content
+         (map element->map)
+         (map (comp (juxt :title :id :author) :best_book :work)))))
